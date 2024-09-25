4aba60e980d4c5ed474d84f4e4ba8ce43a8c6f63
Yashdeep <yashu.deepyadav@gmail.com>
2024-09-26 04:04:59 +0530

    Creating elements via html, javascript and react

---
+++ App.js
@@ -0,0 +1,8 @@
+const h1r = React.createElement("h1", { id: "h1r", className: "h1rcls", props: "91" }, "From REACT")
+
+// h1r is a javascript object
+
+const roo = ReactDOM.createRoot(document.getElementById("root"))
+
+
+roo.render(h1r)
